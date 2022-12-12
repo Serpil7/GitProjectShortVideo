2a83598107a8edab843cf3f15054ea42fd345ae2
Serpil7 Akkaya <erkaya7@hotmail.com>
2022-12-12 00:04:34 +0000

    Added more hello feature

--- src/Work1/Hello.java
+++ src/Work1/Hello.java
@@ -7,6 +7,8 @@ public class Hello {
 
         System.out.println("Created Git Repository");
         System.out.println("Made few commits");
+        System.out.println("Local repo is already published to Github");
+        System.out.println("Doing more work");
 
     }
 }
